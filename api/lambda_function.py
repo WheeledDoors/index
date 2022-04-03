b18a81c1c2ad33ef83e30f795e2a15f4cc249a09
Yoni <yweiner@wpi.edu>
2022-04-03 15:20:53 -0400

    add api things to test with workflow

---
+++ api/lambda_function.py
@@ -0,0 +1,19 @@
+import gensim.downloader
+import json
+
+def lambda_handler(event, context):
+	print("Received event: " + str(event))
+	poc_lambda()
+	return {
+		'statusCode': 200,
+		'body': json.dumps('Hello from Lambda!')
+	}
+
+
+def poc_lambda():
+	glove_vectors = gensim.downloader.load('glove-twitter-200')
+	print("Word2Vec model glove-twitter-200 loaded")
+	print("Door vector: " + glove_vectors['door'])
+	print("Wheel vector: " + glove_vectors['wheel'])
+	print(glove_vectors.most_similar('door'))
+	print(glove_vectors.most_similar('wheel'))
