c0bc2100d50503a0feed47a4671cd549d8cb8a69
Yoni <yweiner@wpi.edu>
2022-04-27 16:39:44 -0400

    update

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from gensim import corpora
-
+import gensim
 
 def handler(event, context):
     print("Received event: " + str(event))
@@ -22,96 +22,39 @@ def handler(event, context):
 
     setup_nltk_dir()
 
-    tres1, rres1, wres1 = search(event['word1'])
-    tres2, rres2, wres2 = search(event['word2'])
+    tres1, rres1, wres1, tnb1, rnb1, tlda1, rlda1  = search(event['word1'])
+    # tres2, rres2, wres2 = search(event['word2'])
 
     data = {}
     data['word1'] = {}
-    data['word2'] = {}
+    # data['word2'] = {}
 
     data['word1']['related_words'] = {}
     data['word1']['related_words']['wiki'] = wres1
     data['word1']['related_words']['twit'] = tres1
-    # data['word1']['related_words']['redd'] = rres1
-    data['word1']['related_words']['redd'] = [('dummy', 0.5)]  # TODO Remove
+    data['word1']['related_words']['redd'] = rres1
+    # data['word1']['related_words']['redd'] = [('dummy', 0.5)]  # TODO Remove
 
-    data['word2']['related_words'] = {}
-    data['word2']['related_words']['wiki'] = wres2
-    data['word2']['related_words']['twit'] = tres2
-    # data['word2']['related_words']['redd'] = rres2
-    data['word2']['related_words']['redd'] = [('dummy', 0.5)]  # TODO Remove
+    # data['word2']['related_words'] = {}
+    # data['word2']['related_words']['wiki'] = wres2
+    # data['word2']['related_words']['twit'] = tres2
+    # # data['word2']['related_words']['redd'] = rres2
+    # data['word2']['related_words']['redd'] = [('dummy', 0.5)]  # TODO Remove
 
     # TODO Change placeholder
-    data['word1']['sentiment'] = [0.75, 0.25, 75, 25, 25, 75]
-    data['word2']['sentiment'] = [0.25, 0.75, 25, 75, 75, 25]
-
-    dummy_topics = {'Topic_0': ['br',
-                                'product',
-                                'would',
-                                'amazon',
-                                'flavor',
-                                'like',
-                                'buy',
-                                'taste',
-                                '2',
-                                'tbsp'],
-                    'Topic_1': ['br',
-                                'tea',
-                                'like',
-                                'one',
-                                'good',
-                                'flavor',
-                                'also',
-                                'make',
-                                'taste',
-                                'magnesium'],
-                    'Topic_2': ['coffee',
-                                'br',
-                                'flavor',
-                                'product',
-                                'cup',
-                                'get',
-                                'would',
-                                'taste',
-                                'good',
-                                'tea'],
-                    'Topic_3': ['br',
-                                'like',
-                                'food',
-                                'taste',
-                                'good',
-                                'love',
-                                'dog',
-                                'great',
-                                'really',
-                                'eat'],
-                    'Topic_4': ['br',
-                                'good',
-                                'coffee',
-                                'product',
-                                'like',
-                                'would',
-                                'store',
-                                'chip',
-                                'great',
-                                'price']}
-
-    dummy_topics2 = dummy_topics.copy()
-    dummy_topics2['Topic_0'][0] = 'weewoo'
-    dummy_topics3 = dummy_topics.copy()
-    dummy_topics3['Topic_0'][0] = 'apple'
-    dummy_topics4 = dummy_topics.copy()
-    dummy_topics4['Topic_0'][0] = 'banana'
+    # data['word1']['sentiment'] = [0.75, 0.25, 75, 25, 25, 75]
+    data['word1']['sentiment'] = [tnb1[0], rnb1[0], tnb1[1], tnb1[2], rnb1[1], rnb1[2]]
+    # data['word2']['sentiment'] = [0.25, 0.75, 25, 75, 75, 25]
 
     data['word1']['topic'] = {}
-    data['word2']['topic'] = {}
+    # data['word2']['topic'] = {}
 
-    data['word1']['topic']['wiki'] = dummy_topics
-    data['word1']['topic']['redd'] = dummy_topics2
-    data['word1']['topic']['twit'] = dummy_topics3
-    data['word2']['topic']['wiki'] = dummy_topics2
-    data['word2']['topic']['redd'] = dummy_topics3
-    data['word2']['topic']['twit'] = dummy_topics4
+    # data['word1']['topic']['wiki'] = wlda1
+    data['word1']['topic']['redd'] = rlda1
+    data['word1']['topic']['twit'] = tlda1
+    # data['word2']['topic']['wiki'] = dummy_topics2
+    # data['word2']['topic']['redd'] = dummy_topics3
+    # data['word2']['topic']['twit'] = dummy_topics4
 
     return {
         'statusCode': 200,
@@ -164,7 +107,7 @@ def setup_nltk_dir():
 
 def setup_df_clean(text_list):
     df = pd.Series(text_list)
-    print(df)
+    # print(df)
     # Remove any rows with NA values
     return df
 
@@ -172,20 +115,23 @@ def setup_df_clean(text_list):
 def search(word):
     print('## SEARCHING FOR: ' + word)
     # Get tweets
+    print("### Getting TWEETS")
     tweets_data = get_tweets(word, 100)
     tweets_text = [x["text"] for x in tweets_data["statuses"]]
 
+    print("### Getting Reddit")
     # Get reddit # TODO Bring back
-    # reddit = praw.Reddit(
-    #     client_id="r3dlLRYKcBN9-JRoyvi1Xw",
-    #     client_secret="YfKwRqhLjS1darC2TL5QnrzDe0S_sQ",
-    #     user_agent="hiiiiiiiiii",
-    # )
-    # reddit_data = reddit.subreddit("all").search(word, limit=1000)
-    # reddit_text = [x.title for x in reddit_data]
+    reddit = praw.Reddit(
+        client_id="r3dlLRYKcBN9-JRoyvi1Xw",
+        client_secret="YfKwRqhLjS1darC2TL5QnrzDe0S_sQ",
+        user_agent="hiiiiiiiiii",
+    )
+    reddit_data = reddit.subreddit("all").search(word, limit=200)
+    reddit_text = [x.title for x in reddit_data]
 
+    print("### Getting Wiki")
     # Get wikipedia
-    wiki_search = get_wikipedia_data(word, 100)
+    wiki_search = get_wikipedia_data(word, 50)
     wiki_text = []
     for page in wiki_search['query']['search']:
         url = 'https://en.wikipedia.org/wiki/' + page['title']
@@ -215,30 +161,35 @@ def search(word):
     ### SETUP SENTIMENT ANALYSIS ###
 
     print('## SENTIMENT ANALYSIS')
-    print(tweets_text)
+    # print(tweets_text)
     # print(wiki_text)
 
     twit_df = setup_df_clean(tweets_text)
-    # reddit_df = setup_df_clean(reddit_text) # TODO Bring back
+    redd_df = setup_df_clean(reddit_text) # TODO Bring back
     wiki_df = setup_df_clean(wiki_text)
 
-    print('## DFs')
-    print(twit_df)
+    # print('## DFs')
+    # print(twit_df)
     # print(reddit_df) # TODO Bring back
     # print(wiki_df)
 
     # Split into sentences
     tweets_text = split_into_sentences(tweets_text)
-    # reddit_text = split_into_sentences(reddit_text) # TODO Bring back
+    reddit_text = split_into_sentences(reddit_text) # TODO Bring back
     wiki_text = split_into_sentences(wiki_text)
 
     ### RUN SENTIMENT THINGS ###
     twit_nb = NB_BOW(twit_df)
-    # redd_nb = NB_BOW(redd_df) # TODO Bring back
+    redd_nb = NB_BOW(redd_df) # TODO Bring back
     wiki_nb = NB_BOW(wiki_df)
 
+    print('## LDA')
+    twit_lda = LDA(twit_df, 3)
+    redd_lda = LDA(redd_df, 3) # TODO Bring back
+    # wiki_lda = LDA(wiki_df, 3)
+
     print(twit_nb)
-    # print(redd_nb) # TODO Bring back
+    print(redd_nb) # TODO Bring back
     print(wiki_nb)
 
     ### RUN IF YOU \ ##
@@ -249,32 +200,32 @@ def search(word):
 
     print("## LOADING MODELS")
     model_twitter = Word2Vec.load("models/base.model")
-    # model_reddit = Word2Vec.load("models/base.model") # TODO Bring back
+    model_reddit = Word2Vec.load("models/base.model") # TODO Bring back
     model_wiki = Word2Vec.load("models/base.model")
 
     print("## TRAINING MODELS")
     # Train the model
     model_twitter.train(
         tweets_text, total_examples=len(tweets_text), epochs=10)
-    # model_reddit.train(reddit_text, total_examples=len(reddit_text), epochs=10) # TODO Bring back
+    model_reddit.train(reddit_text, total_examples=len(reddit_text), epochs=10) # TODO Bring back
     model_wiki.train(wiki_text, total_examples=len(wiki_text), epochs=10)
 
     twitter_top = model_twitter.wv.most_similar(word, topn=10)
-    # reddit_top = model_reddit.wv.most_similar(word, topn=10) # TODO Bring back
+    reddit_top = model_reddit.wv.most_similar(word, topn=10) # TODO Bring back
     wiki_top = model_wiki.wv.most_similar(word, topn=10)
     # print(word + " twitter: " + str(twitter_top))
     # print(word + " reddit: " + str(reddit_top))
     # print(word + " wiki: " + str(wiki_top))
 
     # Print the length of the results
-    print("## LENGTHS")
-    print("Twitter: " + str(len(twitter_top)))
+    # print("## LENGTHS")
+    # print("Twitter: " + str(len(twitter_top)))
     # print("Reddit: " + str(len(reddit_top))) # TODO Bring back
-    print("Wiki: " + str(len(wiki_top)))
+    # print("Wiki: " + str(len(wiki_top)))
 
-    reddit_top = ['aaa']
+    # reddit_top = ['aaa']
 
-    return twitter_top, reddit_top, wiki_top
+    return twitter_top, reddit_top, wiki_top, twit_nb, redd_nb, wiki_nb, twit_lda, redd_lda
 
 
 def clean_data(df):
@@ -306,6 +257,7 @@ def LDA(df, n_topics):
     my_dict = {'Topic_' + str(i): [token for token, score in ldamodel.show_topic(i, topn=10)] for i in
                range(0, ldamodel.num_topics)}
 
+    print(my_dict)
     return my_dict
 
 
